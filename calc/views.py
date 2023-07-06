61607d7d8af506632ca0c9765f13109f7334b2eb
joy1611 <joypatel16n@gmail.com>
2023-07-06 12:20:03 -0400

    Initial commit

---
+++ calc/views.py
@@ -0,0 +1,19 @@
+from django.http import JsonResponse
+from django.shortcuts import render
+
+
+def index(request):
+    comments = [
+        {'name': 'joy patel',
+         'username': '@johnsmith',
+         'text': 'I agree!'},
+        {'name': 'Stacy Warner',
+         'username': '@stacy1994',
+         'text': 'OP, you are the best'}
+    ]
+
+    # context = {'comments' : comments}
+
+    # return render(request, 'index.html', context)
+
+    return JsonResponse({'comments': comments})
